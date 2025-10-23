b4b10505b9d195766bd3b9fd9ee7da1edaa9237f
Bruce Wayne <oakhenry2@gmail.com>
2025-10-23 14:38:42 -0300

    Feat: adding module category

---
+++ src/database/prisma/migrations/20251023173247_first/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "category_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/modules/modules.module.ts
@@ -0,0 +1,2 @@
+
+import { CategoryModule } from './category/category.module';
