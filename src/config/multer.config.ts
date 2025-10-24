1a64711294af4b91a75b4ca1ca244f289cd33181
Bruce Wayne <oakhenry2@gmail.com>
2025-10-24 00:45:13 -0300

    Feat: adding upload image in products modules.

--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -5,18 +5,15 @@ import { extname } from 'path';
 import { randomBytes } from 'crypto';
 import { BadRequestException } from '@nestjs/common';
 
-// Esta função será chamada pelo `useFactory` no módulo
 export const multerConfigFactory = (configService: ConfigService): MulterOptions => {
-  // Lê o destino do .env, com um padrão de './public/uploads'
   const destination = configService.get<string>('MULTER_DEST', './public/uploads');
   console.log('Multer Destination (from factory):', destination)
   
   return {
     limits: {
-      fileSize: +configService.get<number>('MULTER_MAX_SIZE', 5 * 1024 * 1024), // Padrão 5MB
+      fileSize: +configService.get<number>('MULTER_MAX_SIZE', 5 * 1024 * 1024),
     },
 
-    // 2. Filtro de arquivos
     fileFilter: (req, file, callback) => {
       if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
         callback(null, true);
@@ -28,12 +25,11 @@ export const multerConfigFactory = (configService: ConfigService): MulterOptions
       }
     },
 
-    // 3. Storage (com nomes de arquivo customizados)
     storage: diskStorage({
-      destination: destination, // Usa o destino do .env
+      destination: destination, 
       filename: (req, file, callback) => {
-        const fileExtName = extname(file.originalname); // Pega a extensão
-        const randomName = randomBytes(16).toString('hex'); // Gera nome aleatório
+        const fileExtName = extname(file.originalname); 
+        const randomName = randomBytes(16).toString('hex'); 
         callback(null, `${randomName}${fileExtName}`);
       },
     }),
